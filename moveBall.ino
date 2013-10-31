beead1a79bdcba64d96811961d1c5eda5153e1f5
Willian <willianoma@hotmail.com>
2013-10-31 20:48:09 -0300

    Atualizado e insercao do joystick

--- moveBall.ino
+++ moveBall.ino
@@ -2,42 +2,57 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  
+  int pinX = A0;
+  int pinY = A1;
+  int pinSw = 13;
 
+  int x = 0;
+  int y = 0;
+  int sw = 0;
+  
 void setup() {
-  // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
-  //int linha1 = 0;
-  int linha2 = 0;
+  Serial.begin(57600);
+  
+
 
 }
 
 void loop() {
-  
-  Principal();
-  
-  for(int i=1; i<60;i++){
-  setScore(i*52);
-  setTimer(i);
-  delay(200); 
-  }
-  
   lcd.clear();
+  Principal(); 
+  transicao();
+  msgAlerta("fimDeJogo");
   
-}
+//    x = analogRead(pinX);
+//    y = analogRead(pinY);
+//    sw = digitalRead(pinSw);
+//    Serial.println(sw);
+
+  
+} //Fim do loop
 
   void Principal(){
   lcd.setCursor(1,0);
   lcd.print("Score");
   lcd.setCursor(10,0);
   lcd.print("Timer");
-  }
+  
 
+      Serial.println(x);
   
+   for(int i=1; i<=60;i++){    
+    setScore(i*52);
+    setTimer(i);
+    x = analogRead(pinX);
+    delay(x); 
+  }
+     
+  }
+ 
   void setScore(int scoreAndroid){
-  
-   int Score = scoreAndroid;
-  
-  
+     int Score = scoreAndroid;
   if(Score>=0 && Score <10){
     lcd.setCursor(1,1);
     lcd.print("0000");
@@ -66,22 +81,61 @@ void loop() {
     lcd.setCursor(1,1);
     lcd.print(Score);
     }  
-
-  }
+  } //Fim setScore
   
   void setTimer(int timerAndroid){
-  int Timer = timerAndroid;
-  
+    int Timer = timerAndroid;
   if(Timer<10){
     lcd.setCursor(11,1);
     lcd.print(0);
     lcd.setCursor(12,1);
     lcd.print(Timer); 
-    } else{
+    }else{
       lcd.setCursor(11,1);
       lcd.print(Timer);
       }
   
   lcd.setCursor(13,1);
   lcd.print("s");
-  }
+  }// Fim setTime
+  
+  void transicao(){  
+   for (int i=0; i<=16; i++){
+	for (int j=0; j<=1;j++){
+	lcd.setCursor(i,j);
+	lcd.print("*"); 
+        delay(75);
+	}
+ delay(5);
+    }
+    lcd.clear();
+   
+  } //fim transicao
+  
+  
+  void msgAlerta(String msgAndroid){
+    if (msgAndroid=="levelUp"){      
+       efeitoDuasLinhas("Passou de Nivel!");
+        lcd.clear(); 
+    }else
+     if (msgAndroid=="fimDeJogo"){
+      efeitoDuasLinhas("Fim de Jogo!"); 
+       }
+       
+    delay(1500);
+    lcd.clear();
+   }
+ 
+  void efeitoDuasLinhas(String valor){
+  for (int i=0; i<=4; i++){
+	for (int j=0; j<=1;j++){
+	lcd.setCursor(0,j);
+	lcd.print(valor); 
+        delay(400);
+        lcd.clear(); 
+	}
+     }
+  } //fim efeitoDuasLinhas
+  
+  
+  
