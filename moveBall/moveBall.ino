d873c69d0bcc353c7eb8207acfdd1b0b787e5595
willianoma <willian.movel@gmail.com>
2013-11-09 00:09:37 -0300

    testando

--- moveBall/moveBall.ino
+++ moveBall/moveBall.ino
@@ -4,7 +4,7 @@
 MeetAndroid meetAndroid;
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-  
+  //teste
   int pinX = A0;
   int pinY = A1;
   
